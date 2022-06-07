f185eda1ed317c4afbcc5981498ed4c9fd71cdcc
LAPTOP-E8U4FAPL\Koichi <ant19071916@gmail.com>
2022-06-07 21:29:14 +0900

    ADD minimal functionality for todo app(add, delete, check)

--- clojure-todo/src/main/todo/app.cljs
+++ clojure-todo/src/main/todo/app.cljs
@@ -6,9 +6,46 @@
 (defn init []
       (println "App initiated!"))
 
-(println "Code reloaded")
+(defonce counters (r/atom []))
 
-(defn Application []
-      "Hello World!")
+(defonce tasks (r/atom [{:content "example" :done? false}]))
 
-(dom/render [Application] (js/document.getElementById "app"))
+(defn add-task [s]
+      (swap! tasks conj (hash-map :content s, :done? false)))
+
+(defn drop-nth [coll i]
+      (vec (concat (take i coll) (drop (inc i) coll))))
+
+(defn task-input [value]
+      [:input {:type "text"
+               :value @value
+               :on-change #(reset! value (-> % .-target .-value))
+               :placeholder "type task here"}])
+
+(defn add-task-button []
+       (let [val (r/atom "")]
+            (fn []
+                [:div
+                  [task-input val]
+                  " " [:button
+                  {:onClick (fn [] (add-task @val))}
+                  "Add Task"]])) )
+
+(defn App []
+      [:div
+       [:h1 "Tasks"]
+       (doall
+            (for [[i task] (map vector (range) @tasks)]
+                  [:div {:key (str i)}
+                   (task :content)
+                   " " [:button
+                        {:onClick (fn [] (swap! tasks update i (fn [task] (update task :done? #(not %)))))}
+                        "✓"]
+                    " " [:button
+                         {:onClick (fn [] (swap! tasks drop-nth i))}
+                         "X"]]))
+                  [add-task-button (r/atom "")]
+                  ])
+
+
+(dom/render [App] (js/document.getElementById "app"))
